6b2b3eb3ac30d1512a76af33e3838dd8a55bdaba
sharonramirez21 <sharonramirez421@gmail.com>
2026-05-19 19:01:19 -0300

    auth

--- auth.ts
+++ auth.ts
@@ -5,9 +5,9 @@ import { z } from 'zod';
 import type { User } from '@/app/lib/definitions';
 import bcrypt from 'bcrypt';
 import postgres from 'postgres';
- 
+
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
- 
+
 async function getUser(email: string): Promise<User | undefined> {
   try {
     const user = await sql<User[]>`SELECT * FROM users WHERE email=${email}`;
@@ -17,7 +17,7 @@ async function getUser(email: string): Promise<User | undefined> {
     throw new Error('Failed to fetch user.');
   }
 }
- 
+
 export const { auth, signIn, signOut, handlers } = NextAuth({
   ...authConfig,
   providers: [
@@ -26,7 +26,7 @@ export const { auth, signIn, signOut, handlers } = NextAuth({
         const parsedCredentials = z
           .object({ email: z.string().email(), password: z.string().min(6) })
           .safeParse(credentials);
- 
+
         if (parsedCredentials.success) {
           const { email, password } = parsedCredentials.data;
           const user = await getUser(email);
@@ -35,7 +35,7 @@ export const { auth, signIn, signOut, handlers } = NextAuth({
 
           if (passwordsMatch) return user;
         }
-        
+
         console.log('Invalid credentials');
         return null;
       },
